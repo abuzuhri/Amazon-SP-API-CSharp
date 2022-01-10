3348fd367b051acd063d5d9704da02978493f201
Gonzalo Cuadrado <regalooriginal-bugs1@outlook.es>
2022-01-10 13:05:04 +0100

    Mapped OrderFulfillment.xsd to class and integrate it in ConstructFeedService.

--- Source/FikaAmazonAPI/ConstructFeed/ConstructFeedService.cs
+++ Source/FikaAmazonAPI/ConstructFeed/ConstructFeedService.cs
@@ -94,6 +94,23 @@ namespace FikaAmazonAPI.ConstructFeed
             envelope.MessageType = Utils.Constants.FeedMessageType.Product;
         }
 
+        public void AddOrderFulfillmentMessage(IList<OrderFulfillmentMessage> messages, Constants.OperationType operationType = Constants.OperationType.Update)
+        {
+            var msgs = new List<BaseMessage>();
+            int index = 1;
+            foreach (var itm in messages)
+            {
+                msgs.Add(new BaseMessage()
+                {
+                    MessageID = index++,
+                    OrderFulfillment = itm,
+                    OperationType = operationType
+                });
+            }
+            envelope.Message = msgs;
+            envelope.MessageType = Utils.Constants.FeedMessageType.OrderFulfillment;
+        }
+
         public string GetXML()
         {
             return LinqHelper.SerializeObject(envelope);
