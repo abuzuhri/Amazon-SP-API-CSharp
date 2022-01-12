54be037de6d84c74a2aef06417d0cdcd54ef8560
Kevin <venclovas.kevin@googlemail.com>
2022-01-12 11:46:18 +0100

    outsource unauthorized exception to handele relogin

---
+++ Source/FikaAmazonAPI/Utils/ErrorResponseHelper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace FikaAmazonAPI.Utils
+{
+    public static class ErrorResponseHelper
+    {
+        public static ErrorResponse ConvertToErrorResponse(this string response)
+        {
+            if (String.IsNullOrEmpty(response)) return null;
+
+            try
+            {
+                return JsonConvert.DeserializeObject<ErrorResponse>(response);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+
+
+    public class ErrorResponse
+    {
+        [JsonProperty("errors")]
+        public ErrorResponseElement[] Errors;
+    }
+
+    public class ErrorResponseElement
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("code")]
+        public HttpStatusCode Code { get; set; }
+        [JsonProperty("details")]
+        public string Details { get; set; }
+    }
+}
