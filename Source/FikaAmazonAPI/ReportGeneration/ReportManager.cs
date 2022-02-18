342522c264640d2c285284b58ecaa1d6df4ab681
Tareq Abuzuhri <tareq.abuzuhri@gmail.com>
2022-02-18 19:27:49 +0100

    fix reports

--- Source/FikaAmazonAPI/ReportGeneration/ReportManager.cs
+++ Source/FikaAmazonAPI/ReportGeneration/ReportManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using static FikaAmazonAPI.Utils.Constants;
 
 namespace FikaAmazonAPI.ReportGeneration
@@ -16,21 +17,23 @@ namespace FikaAmazonAPI.ReportGeneration
         }
 
         #region feedback
-        public List<FeedbackOrderRow> GetFeedbackFromDays(int days)
+        public List<FeedbackOrderRow> GetFeedbackFromDays(int days) =>
+            GetFeedbackFromDaysAsync(days).GetAwaiter().GetResult();
+        public async Task<List<FeedbackOrderRow>> GetFeedbackFromDaysAsync(int days)
         {
             DateTime fromDate = DateTime.UtcNow.AddDays(-1 * days);
             DateTime toDate = DateTime.UtcNow;
-            return GetFeedbackFromDate(fromDate,toDate);
+            return await GetFeedbackFromDateAsync(fromDate, toDate);
         }
-        public List<FeedbackOrderRow> GetFeedbackFromDate(DateTime fromDate, DateTime toDate)
+        public async Task<List<FeedbackOrderRow>> GetFeedbackFromDateAsync(DateTime fromDate, DateTime toDate)
         {
-            var path = GetFeedbackFromDate(_amazonConnection, fromDate, toDate);
+            var path = await GetFeedbackFromDateAsync(_amazonConnection, fromDate, toDate);
             FeedbackOrderReport report = new FeedbackOrderReport(path, _amazonConnection.RefNumber);
             return report.Data;
         }
-        private string GetFeedbackFromDate(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
+        private async Task<string> GetFeedbackFromDateAsync(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
         {
-            return amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_SELLER_FEEDBACK_DATA, fromDate);
+            return await amazonConnection.Reports.CreateReportAndDownloadFileAsync(ReportTypes.GET_SELLER_FEEDBACK_DATA, fromDate);
         }
 
         #endregion
@@ -39,93 +42,97 @@ namespace FikaAmazonAPI.ReportGeneration
         #endregion
 
         #region Reimbursement
-        public IList<ReimbursementsOrderRow> GetReimbursementsOrder(int days)
+        public IList<ReimbursementsOrderRow> GetReimbursementsOrder(int days) =>
+            GetReimbursementsOrderAsync(days).GetAwaiter().GetResult();
+        public async Task<IList<ReimbursementsOrderRow>> GetReimbursementsOrderAsync(int days)
         {
             DateTime fromDate = DateTime.UtcNow.AddDays(-1 * days);
             DateTime toDate = DateTime.UtcNow;
-            return GetReimbursementsOrder(fromDate, toDate);
+            return await GetReimbursementsOrderAsync(fromDate, toDate);
         }
-        public IList<ReimbursementsOrderRow> GetReimbursementsOrder(DateTime fromDate, DateTime toDate)
+        public async Task<IList<ReimbursementsOrderRow>> GetReimbursementsOrderAsync(DateTime fromDate, DateTime toDate)
         {
-            var path = GetReimbursementsOrder(_amazonConnection, fromDate, toDate);
+            var path = await GetReimbursementsOrderAsync(_amazonConnection, fromDate, toDate);
             ReimbursementsOrderReport report = new ReimbursementsOrderReport(path, _amazonConnection.RefNumber);
             return report.Data;
         }
 
-        private string GetReimbursementsOrder(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
+        private async Task<string> GetReimbursementsOrderAsync(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
         {
-            return amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_FBA_REIMBURSEMENTS_DATA, fromDate, toDate);
+            return await amazonConnection.Reports.CreateReportAndDownloadFileAsync(ReportTypes.GET_FBA_REIMBURSEMENTS_DATA, fromDate, toDate);
         }
-
-
         #endregion
 
         #region ReturnFBAOrder
-        public List<ReturnFBAOrderRow> GetReturnFBAOrder(int days)
+        public List<ReturnFBAOrderRow> GetReturnFBAOrder(int days) =>
+            GetReturnFBAOrderAsync(days).GetAwaiter().GetResult();
+        public async Task<List<ReturnFBAOrderRow>> GetReturnFBAOrderAsync(int days)
         {
             DateTime fromDate = DateTime.UtcNow.AddDays(-1 * days);
             DateTime toDate = DateTime.UtcNow;
-            return GetReturnFBAOrder(fromDate, toDate);
+            return await GetReturnFBAOrderAsync(fromDate, toDate);
         }
-        public List<ReturnFBAOrderRow> GetReturnFBAOrder(DateTime fromDate, DateTime toDate)
+        public async Task<List<ReturnFBAOrderRow>> GetReturnFBAOrderAsync(DateTime fromDate, DateTime toDate)
         {
-            var path = GetReturnFBAOrder(_amazonConnection, fromDate, toDate);
+            var path = await GetReturnFBAOrderAsync(_amazonConnection, fromDate, toDate);
             ReturnFBAOrderReport report = new ReturnFBAOrderReport(path, _amazonConnection.RefNumber);
-  
+
             return report.Data;
         }
 
-        private string GetReturnFBAOrder(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
+        private async Task<string> GetReturnFBAOrderAsync(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
         {
-            return amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA, fromDate, toDate);
+            return await amazonConnection.Reports.CreateReportAndDownloadFileAsync(ReportTypes.GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA, fromDate, toDate);
         }
 
 
         #endregion
 
         #region ReturnFBMOrder
-        public List<ReturnFBMOrderRow> GetReturnMFNOrder(int days)
+        public List<ReturnFBMOrderRow> GetReturnMFNOrder(int days) =>
+            GetReturnMFNOrderAsync(days).GetAwaiter().GetResult();
+        public async Task<List<ReturnFBMOrderRow>> GetReturnMFNOrderAsync(int days)
         {
             DateTime fromDate = DateTime.UtcNow.AddDays(-1 * days);
             DateTime toDate = DateTime.UtcNow;
-            return GetReturnMFNOrder(fromDate, toDate);
+            return await GetReturnMFNOrderAsync(fromDate, toDate);
         }
-        public List<ReturnFBMOrderRow> GetReturnMFNOrder(DateTime fromDate, DateTime toDate)
+        public async Task<List<ReturnFBMOrderRow>> GetReturnMFNOrderAsync(DateTime fromDate, DateTime toDate)
         {
-            List<ReturnFBMOrderRow> list=new List<ReturnFBMOrderRow>();
+            List<ReturnFBMOrderRow> list = new List<ReturnFBMOrderRow>();
             var dateList = ReportDateRange.GetDateRange(fromDate, toDate, DAY_60);
             foreach (var date in dateList)
             {
-                var path = GetReturnMFNOrder(_amazonConnection, date.StartDate, date.EndDate);
+                var path = await GetReturnMFNOrderAsync(_amazonConnection, date.StartDate, date.EndDate);
 
                 ReturnFBMOrderReport report = new ReturnFBMOrderReport(path, _amazonConnection.RefNumber);
                 list.AddRange(report.Data);
             }
             return list;
         }
-        public string GetReturnMFNOrder(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
+        public async Task<string> GetReturnMFNOrderAsync(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
         {
-            return amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_FLAT_FILE_RETURNS_DATA_BY_RETURN_DATE, fromDate, toDate);
+            return await amazonConnection.Reports.CreateReportAndDownloadFileAsync(ReportTypes.GET_FLAT_FILE_RETURNS_DATA_BY_RETURN_DATE, fromDate, toDate);
         }
-
-
         #endregion
 
         #region Settlement
-        public List<SettlementOrderRow> GetSettlementOrder(int days)
+        public List<SettlementOrderRow> GetSettlementOrder(int days) =>
+            GetSettlementOrderAsync(days).GetAwaiter().GetResult();
+        public async Task<List<SettlementOrderRow>> GetSettlementOrderAsync(int days)
         {
             DateTime fromDate = DateTime.UtcNow.AddDays(-1 * days);
             DateTime toDate = DateTime.UtcNow;
-            return GetSettlementOrder(fromDate, toDate);
+            return await GetSettlementOrderAsync(fromDate, toDate);
         }
-        public List<SettlementOrderRow> GetSettlementOrder(DateTime fromDate, DateTime toDate)
+        public async Task<List<SettlementOrderRow>> GetSettlementOrderAsync(DateTime fromDate, DateTime toDate)
         {
             List<SettlementOrderRow> list = new List<SettlementOrderRow>();
             var totalDays = (DateTime.UtcNow - fromDate).TotalDays;
             if (totalDays > 90)
                 fromDate = DateTime.UtcNow.AddDays(-90);
 
-            var paths = GetSettlementOrder(_amazonConnection, fromDate, toDate);
+            var paths = await GetSettlementOrderAsync(_amazonConnection, fromDate, toDate);
             foreach (var path in paths)
             {
                 SettlementOrderReport report = new SettlementOrderReport(path, _amazonConnection.RefNumber);
@@ -134,9 +141,9 @@ namespace FikaAmazonAPI.ReportGeneration
 
             return list;
         }
-        private IList<string> GetSettlementOrder(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
+        private async Task<IList<string>> GetSettlementOrderAsync(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
         {
-            return amazonConnection.Reports.DownloadExistingReportAndDownloadFile(ReportTypes.GET_V2_SETTLEMENT_REPORT_DATA_FLAT_FILE_V2, fromDate, toDate);
+            return await amazonConnection.Reports.DownloadExistingReportAndDownloadFileAsync(ReportTypes.GET_V2_SETTLEMENT_REPORT_DATA_FLAT_FILE_V2, fromDate, toDate);
         }
         #endregion
 
@@ -153,76 +160,84 @@ namespace FikaAmazonAPI.ReportGeneration
         #endregion
 
         #region GetInventoryAging
-        public List<InventoryAgingRow> GetInventoryAging()
+        public List<InventoryAgingRow> GetInventoryAging() =>
+            GetInventoryAgingAsync().GetAwaiter().GetResult();
+        public async Task<List<InventoryAgingRow>> GetInventoryAgingAsync()
         {
-            var path = GetInventoryAging(_amazonConnection);
+            var path = await GetInventoryAgingAsync(_amazonConnection);
             InventoryAgingReport report = new InventoryAgingReport(path, _amazonConnection.RefNumber);
             return report.Data;
         }
-        private string GetInventoryAging(AmazonConnection amazonConnection)
+        private async Task<string> GetInventoryAgingAsync(AmazonConnection amazonConnection)
         {
-            return amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_FBA_INVENTORY_AGED_DATA);
+            return await amazonConnection.Reports.CreateReportAndDownloadFileAsync(ReportTypes.GET_FBA_INVENTORY_AGED_DATA);
         }
         #endregion
 
         #region Products
-        public List<ProductsRow> GetProducts()
+        public List<ProductsRow> GetProducts() =>
+            GetProductsAsync().GetAwaiter().GetResult();
+        public async Task<List<ProductsRow>> GetProductsAsync()
         {
-            var path = GetProducts(_amazonConnection);
+            var path = await GetProductsAsync(_amazonConnection);
             ProductsReport report = new ProductsReport(path, _amazonConnection.RefNumber);
             return report.Data;
         }
-        private string GetProducts(AmazonConnection amazonConnection)
+        private async Task<string> GetProductsAsync(AmazonConnection amazonConnection)
         {
-            return amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_MERCHANT_LISTINGS_ALL_DATA);
+            return await amazonConnection.Reports.CreateReportAndDownloadFileAsync(ReportTypes.GET_MERCHANT_LISTINGS_ALL_DATA);
         }
         #endregion
 
         #region Orders
-        public List<OrdersRow> GetOrdersByLastUpdate(int days)
+        public List<OrdersRow> GetOrdersByLastUpdate(int days) =>
+            GetOrdersByLastUpdateAsync(days).GetAwaiter().GetResult();
+        public async Task<List<OrdersRow>> GetOrdersByLastUpdateAsync(int days)
         {
             DateTime fromDate = DateTime.UtcNow.AddDays(-1 * days);
             DateTime toDate = DateTime.UtcNow;
-            return GetOrdersByLastUpdate(fromDate, toDate);
+            return await GetOrdersByLastUpdateAsync(fromDate, toDate);
         }
-        public List<OrdersRow> GetOrdersByLastUpdate(DateTime fromDate, DateTime toDate)
+        public async Task<List<OrdersRow>> GetOrdersByLastUpdateAsync(DateTime fromDate, DateTime toDate)
         {
             List<OrdersRow> list = new List<OrdersRow>();
             var dateList = ReportDateRange.GetDateRange(fromDate, toDate, DAY_30);
             foreach (var range in dateList)
             {
-                var path = GetOrdersByLastUpdate(_amazonConnection, range.StartDate, range.EndDate);
+                var path = await GetOrdersByLastUpdateAsync(_amazonConnection, range.StartDate, range.EndDate);
                 OrdersReport report = new OrdersReport(path, _amazonConnection.RefNumber);
                 list.AddRange(report.Data);
             }
             return list;
         }
-        private string GetOrdersByLastUpdate(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
+        private async Task<string> GetOrdersByLastUpdateAsync(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
         {
-            return amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_FLAT_FILE_ALL_ORDERS_DATA_BY_LAST_UPDATE_GENERAL, fromDate, toDate);
+            return await amazonConnection.Reports.CreateReportAndDownloadFileAsync(ReportTypes.GET_FLAT_FILE_ALL_ORDERS_DATA_BY_LAST_UPDATE_GENERAL, fromDate, toDate);
         }
 
-        public List<OrdersRow> GetOrdersByOrderDate(int days)
+        public List<OrdersRow> GetOrdersByOrderDate(int days) =>
+            GetOrdersByOrderDateAsync(days).GetAwaiter().GetResult();
+        public async Task<List<OrdersRow>> GetOrdersByOrderDateAsync(int days)
         {
             DateTime fromDate = DateTime.UtcNow.AddDays(-1 * days);
             DateTime toDate = DateTime.UtcNow;
-            return GetOrdersByOrderDate(fromDate,toDate);
+            return await GetOrdersByOrderDateAsync(fromDate, toDate);
         }
-        public List<OrdersRow> GetOrdersByOrderDate(DateTime fromDate, DateTime toDate)
+        public async Task<List<OrdersRow>> GetOrdersByOrderDateAsync(DateTime fromDate, DateTime toDate)
         {
             List<OrdersRow> list = new List<OrdersRow>();
-            var dateList=ReportDateRange.GetDateRange(fromDate, toDate,DAY_30);
+            var dateList = ReportDateRange.GetDateRange(fromDate, toDate, DAY_30);
             foreach (var range in dateList)
             {
-                var path = GetOrdersByOrderDate(_amazonConnection, range.StartDate, range.EndDate);
+                var path = await GetOrdersByOrderDateAsync(_amazonConnection, range.StartDate, range.EndDate);
                 OrdersReport report = new OrdersReport(path, _amazonConnection.RefNumber);
                 list.AddRange(report.Data);
             }
             return list;
         }
-        private string GetOrdersByOrderDate(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
+        private async Task<string> GetOrdersByOrderDateAsync(AmazonConnection amazonConnection, DateTime fromDate, DateTime toDate)
         {
-            return amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL, fromDate, toDate);
+            return await amazonConnection.Reports.CreateReportAndDownloadFileAsync(ReportTypes.GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL, fromDate, toDate);
         }
         #endregion
 
