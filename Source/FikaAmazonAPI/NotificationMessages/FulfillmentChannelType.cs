1b2f4693849b1d4ca696fcd956da7b9ff40cbc9b
Luca Rinaldi <info@crinetech.it>
2022-06-30 14:03:00 +0200

    Changed FulfillmentChannelType visibility to public

--- Source/FikaAmazonAPI/NotificationMessages/FulfillmentChannelType.cs
+++ Source/FikaAmazonAPI/NotificationMessages/FulfillmentChannelType.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace FikaAmazonAPI.NotificationMessages
 {
-    internal class FulfillmentChannelType
+    public class FulfillmentChannelType
     {
         public const string Amazon = "Amazon";
         public const string Merchant = "Merchant";
