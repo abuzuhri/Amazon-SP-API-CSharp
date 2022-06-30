970a3248a88d04aec45f88ba6d9318c19e86d3b3
Luca Rinaldi <info@crinetech.it>
2022-06-30 13:55:07 +0200

    Added json info and summary to B2BAnyOfferChangedNotification

--- Source/FikaAmazonAPI/NotificationMessages/B2BAnyOfferChangedNotification.cs
+++ Source/FikaAmazonAPI/NotificationMessages/B2BAnyOfferChangedNotification.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace FikaAmazonAPI.NotificationMessages
 {
     public partial class B2BAnyOfferChangedNotification
     {
+        /// <summary>
+        /// Required. The seller identifier for the offer.
+        /// </summary>
+        [JsonProperty("SellerId")]
         public string SellerId { get; set; }
+
+        /// <summary>
+        /// Required. The event that caused the notification to be sent.
+        /// </summary>
+        [JsonProperty("OfferChangeTrigger")]
         public OfferChangeTrigger OfferChangeTrigger { get; set; }
+
+        /// <summary>
+        /// Required. Information about the product that had the offer change. The information in this summary applies to all conditions of the product.
+        /// </summary>
+        [JsonProperty("Summary")]
         public Summary Summary { get; set; }
+
+        /// <summary>
+        /// Required. The top 20 competitive B2B offers for the item and condition that triggered the notification.
+        /// </summary>
+        [JsonProperty("Offers")]
         public List<Offer> Offers { get; set; }
     }
 
