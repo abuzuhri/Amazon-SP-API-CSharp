48bda8ca55c8625dc4ed7864cd886f72b465cb02
Luca Rinaldi <luca.rinaldi@crinetech.it>
2022-06-30 10:09:16 +0200

    Updated NotificationMessages.Offer and NotificationMessages.OfferElement
    Based on documentation aviable at the following link
    https://spapi.cyou/en/use-case/notifications-use-case-guide-v1.html
    
    The OfferElement class used in the ANY_OFFER_CHANGED notification have almost all properties in common with the Offer class used in B2B_ANY_OFFER_CHANGED notification.
    
    I've reordered the properties in the same order that is on the documentation, added the missing ConditionNotes property and removed the non existent ShipsInternationally property.
    
    After that, i've moved all the common properties into Offer class and make OfferElement a descendant of the Offer class
    
    In this way we can create reusable code that can work for both the ANY_OFFER_CHANGED and B2B_ANY_OFFER_CHANGED classes

--- Source/FikaAmazonAPI/NotificationMessages/Offer.cs
+++ Source/FikaAmazonAPI/NotificationMessages/Offer.cs
@@ -1,18 +1,81 @@
 ﻿namespace FikaAmazonAPI.NotificationMessages
 {
+
+    /// <summary>
+    /// An explanation about the purpose of this instance.
+    /// </summary>
     public partial class Offer
     {
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("SellerId")]
         public string SellerId { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("SubCondition")]
         public string SubCondition { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("SellerFeedbackRating")]
         public SellerFeedbackRating SellerFeedbackRating { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("ShippingTime")]
         public ShippingTime ShippingTime { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("ListingPrice")]
         public MoneyType ListingPrice { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("Shipping")]
         public MoneyType Shipping { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("ShipsFrom")]
         public ShipsFrom ShipsFrom { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("IsFulfilledByAmazon")]
         public bool IsFulfilledByAmazon { get; set; }
+
+        /// <summary>
+        /// Indicates if the offer is buy box winner
+        /// </summary>
+        [JsonProperty("IsBuyBoxWinner")]
         public bool IsBuyBoxWinner { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("ConditionNotes")]
         public string ConditionNotes { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("PrimeInformation")]
         public PrimeInformation PrimeInformation { get; set; }
+
+        /// <summary>
+        /// An explanation about the purpose of this instance.
+        /// </summary>
+        [JsonProperty("IsFeaturedMerchant")]
         public bool IsFeaturedMerchant { get; set; }
     }
 }
--- Source/FikaAmazonAPI/NotificationMessages/OfferElement.cs
+++ Source/FikaAmazonAPI/NotificationMessages/OfferElement.cs
@@ -6,38 +6,8 @@ namespace FikaAmazonAPI.NotificationMessages
     /// <summary>
     /// An explanation about the purpose of this instance.
     /// </summary>
-    public partial class OfferElement
+    public partial class OfferElement : Offer
     {
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("IsExpeditedShippingAvailable")]
-        public bool IsExpeditedShippingAvailable { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("IsFeaturedMerchant")]
-        public bool IsFeaturedMerchant { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("IsFulfilledByAmazon")]
-        public bool IsFulfilledByAmazon { get; set; }
-
-        /// <summary>
-        /// Indicates if the offer is buy box winner
-        /// </summary>
-        [JsonProperty("IsBuyBoxWinner")]
-        public bool IsBuyBoxWinner { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("ListingPrice")]
-        public MoneyType ListingPrice { get; set; }
-
         /// <summary>
         /// An explanation about the purpose of this instance.
         /// </summary>
@@ -47,55 +17,13 @@ namespace FikaAmazonAPI.NotificationMessages
         /// <summary>
         /// An explanation about the purpose of this instance.
         /// </summary>
-        [JsonProperty("PrimeInformation")]
-        public PrimeInformation PrimeInformation { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("SellerFeedbackRating")]
-        public SellerFeedbackRating SellerFeedbackRating { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("SellerId")]
-        public string SellerId { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("Shipping")]
-        public MoneyType Shipping { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("ShippingTime")]
-        public ShippingTime ShippingTime { get; set; }
+        [JsonProperty("IsExpeditedShippingAvailable")]
+        public bool IsExpeditedShippingAvailable { get; set; }
 
         /// <summary>
         /// An explanation about the purpose of this instance.
         /// </summary>
         [JsonProperty("ShipsDomestically")]
         public bool ShipsDomestically { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("ShipsFrom")]
-        public ShipsFrom ShipsFrom { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("ShipsInternationally")]
-        public bool ShipsInternationally { get; set; }
-
-        /// <summary>
-        /// An explanation about the purpose of this instance.
-        /// </summary>
-        [JsonProperty("SubCondition")]
-        public string SubCondition { get; set; }
     }
 }
