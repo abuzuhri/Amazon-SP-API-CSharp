c6373b6d9cba9215127a1ea60d4323d39eac9152
Paul Barrett <paul.barrett@corp.trendnation.com>
2022-07-08 10:31:03 -0700

    merge

--- Source/FikaAmazonAPI.SampleCode/FulFillmentInboundSample.cs
+++ Source/FikaAmazonAPI.SampleCode/FulFillmentInboundSample.cs
@@ -1,10 +1,12 @@
 ﻿using FikaAmazonAPI.Utils;
+using FikaAmazonAPI.Services;
 using FikaAmazonAPI.AmazonSpApiSDK.Models.FulfillmentInbound;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace FikaAmazonAPI.SampleCode
 {
@@ -15,7 +17,7 @@ namespace FikaAmazonAPI.SampleCode
         {
             this.amazonConnection = amazonConnection;
         }
-
+        
         public InboundShipmentList GetInboundShipments()
         {
             var shipments = new InboundShipmentList();
@@ -32,7 +34,12 @@ namespace FikaAmazonAPI.SampleCode
             {
                 results = amazonConnection.FulFillmentInbound.GetShipments(param);
                 shipments.AddRange(results.ShipmentData);
+                Console.WriteLine(results.NextToken);
                 param.NextToken = results.NextToken;
+                foreach (var i in results.ShipmentData.Take(5))
+                {
+                    Console.WriteLine($"{i.ShipmentId} {i.ShipmentStatus}");
+                }
                 Thread.Sleep(10000);
             } while (String.IsNullOrEmpty(results.NextToken) != true);
 
@@ -41,10 +48,12 @@ namespace FikaAmazonAPI.SampleCode
         }
         public void GetInboundGuidance()
         {
-            var parm = new Parameter.FulFillmentInbound.ParameterGetInboundGuidance() {
-                MarketplaceId= MarketPlace.UnitedArabEmirates.ID,
-                ASINList=new List<string> { "B071XVSXRL" }
+            var parm = new Parameter.FulFillmentInbound.ParameterGetInboundGuidance()
+            {
+                MarketplaceId = MarketPlace.UnitedArabEmirates.ID,
+                ASINList = new List<string> { "B071XVSXRL" }
             };
+
             amazonConnection.FulFillmentInbound.GetInboundGuidance(parm);
         }
         
