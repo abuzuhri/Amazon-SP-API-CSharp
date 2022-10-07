7ea3aa8ecc3ec155ed5f2f779eef98114452e9d9
Simon Dingley <simon@prolificnotion.co.uk>
2022-10-07 07:15:26 +0100

    Adds documentation of properties in Products Report
    Taken from the reference at https://sellercentral.amazon.co.uk/help/hub/reference/external/200385890

--- Source/FikaAmazonAPI/ReportGeneration/ProductsReport.cs
+++ Source/FikaAmazonAPI/ReportGeneration/ProductsReport.cs
@@ -25,32 +25,118 @@ namespace FikaAmazonAPI.ReportGeneration
     }
     public class ProductsRow
     {
+        /// <summary>
+        /// The title of the item listed. This will be populated with Amazon catalogue data.
+        /// </summary>
         public string ItemName { get; set; }
+        /// <summary>
+        /// This is used for backwards compatibility only.
+        /// </summary>
         public string ItemDescription { get; set; }
+        /// <summary>
+        /// An identifier created by Amazon when you create a listing. Consists of 4 digits, a capital letter and 6 more digits.
+        /// </summary>
         public string ListingId { get; set; }
+        /// <summary>
+        /// Stock Keeping Units (SKUs) are unique blocks of letters and/or numbers that identify your products.
+        /// SKUs are assigned by you as the seller.
+        /// </summary>
         public string SellerSku { get; set; }
+        /// <summary>
+        /// The price you are asking for the product.
+        /// </summary>
         public decimal? Price { get; set; }
+        /// <summary>
+        /// The quantity of the product available for purchase.
+        /// </summary>
         public int? Quantity { get; set; }
+        /// <summary>
+        /// The date the listing was created.
+        /// </summary>
         public string OpenDate { get; set; }
         public string ImageUrl { get; set; }
+        /// <summary>
+        /// This will always be y.
+        /// </summary>
         public bool ItemIsMarketplace { get; set; }
+        /// <summary>
+        /// A numeric entry that indicates if the product-id is an ASIN, ISBN, UPC or EAN code/number. When uploading files in Standard Book format, this field value will default to 2 (ISBN) unless otherwise specified.
+        /// 1 = ASIN, 2 = ISBN, 3 = UPC, 4 = EAN
+        /// </summary>
         public int? ProductIdType { get; set; }
+        /// <summary>
+        /// This is used for backwards compatibility only.
+        /// </summary>
         public string ZshopShippingFee { get; set; }
+        /// <summary>
+        /// Your description of the listing
+        /// </summary>
+        /// <example>
+        /// Read once, no scratches, normal condition
+        /// </example>
         public string ItemNote { get; set; }
+        /// <summary>
+        /// This is your condition code for the listing.
+        /// 1 = Used; Like New, 2 = Used; Very Good, 3 = Used; Good, 4 = Used; Acceptable, 5 = Collectible; Like New,
+        /// 6 = Collectible; Very Good, 7 = Collectible; Good, 8 = Collectible; Acceptable, 11 = New
+        /// </summary>
         public int? ItemCondition { get; set; }
+        /// <summary>
+        /// This is used for backwards compatibility only.
+        /// </summary>
         public string ZshopCategory1 { get; set; }
+        /// <summary>
+        /// This is used for backwards compatibility only.
+        /// </summary>
         public string ZshopBrowsePath { get; set; }
+        /// <summary>
+        /// This is used for backwards compatibility only.
+        /// </summary>
         public string ZshopStorefrontFeature { get; set; }
+        /// <summary>
+        /// This is not used. The field is populated with the product-id.
+        /// </summary>
         public string ASIN1 { get; set; }
+        /// <summary>
+        /// This is used for backwards compatibility only.
+        /// </summary>
         public string ASIN2 { get; set; }
+        /// <summary>
+        /// This is used for backwards compatibility only.
+        /// </summary>
         public string ASIN3 { get; set; }
+        /// <summary>
+        /// y = listings that are available to customers outside the UK.
+        /// n = listings that are available only to customers in the UK.
+        /// </summary>
         public string WillShipInternationally { get; set; }
+        /// <summary>
+        /// This will always be n
+        /// </summary>
         public string ExpeditedShipping { get; set; }
+        /// <summary>
+        /// This is used for backwards compatibility only.
+        /// </summary>
         public string ZshopBoldface { get; set; }
+        /// <summary>
+        /// ASIN, ISBN, UPC or EAN for the item
+        /// </summary>
         public string ProductId { get; set; }
+        /// <summary>
+        /// This is used for backwards compatibility only.
+        /// </summary>
         public string BidForFeaturedPlacement { get; set; }
+        /// <summary>
+        /// This field will be empty in the report; it enables you to use an open listings report to update online inventory.
+        /// </summary>
         public string AddDelete { get; set; }
+        /// <summary>
+        /// This is the quantity of the item that is pending purchase at the time of the report.
+        /// </summary>
         public int? PendingQuantity { get; set; }
+        /// <summary>
+        /// This will indicate for FBA sellers if the listing is set for seller fulfilment or fulfilment by Amazon.
+        /// </summary>
         public string FulfillmentChannel { get; set; }
         public string OptionalPaymentTypeExclusion { get; set; }
         public string Status { get; set; }
