7e751c6e79d16490fdccc2c142a2be89effebf73
Tareq Abuzuhri <tareq.abuzuhri@gmail.com>
2022-12-23 13:26:03 +0100

    marketplace id

--- Source/FikaAmazonAPI.SampleCode/Program.cs
+++ Source/FikaAmazonAPI.SampleCode/Program.cs
@@ -1,12 +1,5 @@
-﻿using FikaAmazonAPI.ConstructFeed;
-using FikaAmazonAPI.ConstructFeed.Messages;
-using FikaAmazonAPI.Parameter.Finance;
-using FikaAmazonAPI.Parameter.ListingItem;
-using FikaAmazonAPI.Parameter.ListingsItems;
-using FikaAmazonAPI.ReportGeneration;
-using FikaAmazonAPI.Utils;
+﻿using FikaAmazonAPI.ReportGeneration;
 using Microsoft.Extensions.Configuration;
-using static FikaAmazonAPI.AmazonSpApiSDK.Models.ListingsItems.ListingsItemPutRequest;
 using static FikaAmazonAPI.Utils.Constants;
 
 namespace FikaAmazonAPI.SampleCode
@@ -30,12 +23,25 @@ namespace FikaAmazonAPI.SampleCode
                 ClientId = config.GetSection("FikaAmazonAPI:ClientId").Value,
                 ClientSecret = config.GetSection("FikaAmazonAPI:ClientSecret").Value,
                 RefreshToken = config.GetSection("FikaAmazonAPI:RefreshToken").Value,
-                MarketPlace = MarketPlace.GetMarketPlaceByID(config.GetSection("FikaAmazonAPI:MarketPlaceID").Value),
+                MarketPlaceID = config.GetSection("FikaAmazonAPI:MarketPlaceID").Value,
                 IsDebugMode = true
             });
 
+
+            var offers = amazonConnection.ProductPricing.GetItemOffers(new Parameter.ProductPricing.ParameterGetItemOffers
+            {
+                Asin = "B004Z00S8U",//"B07MW2SSZD",// "B0026XRFY8",
+                CustomerType = CustomerType.Consumer,
+                ItemCondition = ItemCondition.New,
+            });
+
+
+
+
+            //B0026XRFY8
+
             ReportManager reportManager1 = new ReportManager(amazonConnection);
-           var list=await reportManager1.GetLedgerDetailAsync(10);
+            var list = await reportManager1.GetLedgerDetailAsync(10);
 
 
             //var data2220002 = amazonConnection.Reports.CreateReportAndDownloadFile(
