5ffbbe07639a3b4e3d05b652bc9d6a5ee8141aa3
dev_chenjiawen <chenjiawen@bestwo.com>
2023-03-13 15:16:50 +0800

    add RestClientSingleton

--- Source/FikaAmazonAPI/AmazonSpApiSDK/Runtime/RestClientSingleton.cs
+++ Source/FikaAmazonAPI/AmazonSpApiSDK/Runtime/RestClientSingleton.cs
@@ -1,24 +1,45 @@
 using System;
 using System.Net.Http;
 using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
 
 namespace FikaAmazonAPI.AmazonSpApiSDK.Runtime
 {
-    public class RestClientSingleton
+    /// <summary>
+    /// Get Singleton HttpClient
+    /// to fixed: RestClient not properly disposed causes memory / socket leaks
+    /// <see cref="https://github.com/abuzuhri/Amazon-SP-API-CSharp/issues/523"/>
+    /// </summary>
+    public static class RestClientSingleton
     {
-        private static readonly HttpClient _httpClient = new HttpClient(new StandardSocketsHttpHandler
-        {
-            PooledConnectionLifetime = TimeSpan.FromMinutes(15)
-        });
+        private static readonly HttpClient _httpClient = null;
 
         static RestClientSingleton()
         {
+            _httpClient = new HttpClient(new StandardSocketsHttpHandler
+            {
+                PooledConnectionLifetime = TimeSpan.FromMinutes(1)
+            });
         }
 
-        private RestClientSingleton()
+        /// <summary>
+        /// Get RestClient By Singleton HttpClient
+        /// </summary>
+        /// <param name="baseUrl"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static RestClient GetRestClient(string baseUrl)
         {
-        }
+            if (!Uri.TryCreate(baseUrl, UriKind.Absolute, out var baseUri))
+            {
+                throw new ArgumentException($"argument of {baseUrl} is illegal");
+            }
 
-        public static RestClient RestClient => new RestClient(_httpClient);
+            var restClientOption = new RestClientOptions
+            {
+                BaseUrl = baseUri,
+            };
+            return new RestClient(_httpClient, restClientOption).UseNewtonsoftJson();
+        }
     }
 }
