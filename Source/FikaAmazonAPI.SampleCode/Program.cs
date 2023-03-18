fbdfd81de10069a577bd274a369c08ea33c0d1bc
Tareq Abuzuhri <tareq.abuzuhri@gmail.com>
2023-03-18 15:50:18 +0100

    sample

--- Source/FikaAmazonAPI.SampleCode/Program.cs
+++ Source/FikaAmazonAPI.SampleCode/Program.cs
@@ -1,4 +1,4 @@
-﻿using FikaAmazonAPI.Parameter.Order;
+﻿using FikaAmazonAPI.Parameter.ListingItem;
 using Microsoft.Extensions.Configuration;
 using static FikaAmazonAPI.Utils.Constants;
 
@@ -24,19 +24,109 @@ namespace FikaAmazonAPI.SampleCode
                 ClientSecret = config.GetSection("FikaAmazonAPI:ClientSecret").Value,
                 RefreshToken = config.GetSection("FikaAmazonAPI:RefreshToken").Value,
                 MarketPlaceID = config.GetSection("FikaAmazonAPI:MarketPlaceID").Value,
+                SellerID = config.GetSection("FikaAmazonAPI:SellerId").Value,
                 IsDebugMode = true
             });
 
+            var search = amazonConnection.ProductType.SearchDefinitionsProductTypes(new Parameter.ProductTypes.SearchDefinitionsProductTypesParameter
+            {
+                keywords = new[] { "pet" }
+            });
 
-            ParameterOrderList serachOrderList = new ParameterOrderList();
-            serachOrderList.CreatedAfter = DateTime.UtcNow.AddMinutes(-6000);
+            var def = amazonConnection.ProductType.GetDefinitionsProductType(
+               new Parameter.ProductTypes.GetDefinitionsProductTypeParameter()
+               {
+                   productType = search.ProductTypes?.FirstOrDefault()?.Name ?? "",
+                   locale = AmazonSpApiSDK.Models.ProductTypes.LocaleEnum.en_US,
 
-            serachOrderList.OrderStatuses = new List<OrderStatuses>();
-            serachOrderList.OrderStatuses.Add(OrderStatuses.Shipped);
+               });
 
-            serachOrderList.AmazonOrderIds = new List<string>();
 
-            var orders = amazonConnection.Orders.GetOrders(serachOrderList);
+            var x = amazonConnection.CatalogItem.GetCatalogItem202204(new FikaAmazonAPI.Parameter.CatalogItems.ParameterGetCatalogItem()
+            {
+                ASIN = "B01I3JW7PK",
+                includedData = new List<IncludedData> {
+               IncludedData.summaries,
+               IncludedData.identifiers,
+               IncludedData.summaries,
+               IncludedData.productTypes,
+               IncludedData.dimensions,
+               IncludedData.images,
+               IncludedData.relationships,
+               IncludedData.attributes//,
+               
+               }
+            });
+            x.Attributes.Remove("street_date");
+            x.Attributes.Add("storage_instructions", new[] { new {
+                    value="not applicable"//, unit="Hertz"
+
+                }});
+            x.Attributes.Add("number_of_boxes", new[] { new {
+                    value=1//, unit="Hertz"
+
+                }});
+
+            x.Attributes.Add("country_of_origin", new[] { new {
+                    value= "UK"//, unit="Hertz"
+
+                }});
+
+            x.Attributes.Add("use_by_recommendation", new[] { new {
+                    value= "not applicable"//, unit="Hertz"
+
+                }});
+
+            x.Attributes.Add("product_description", new[] { new {
+                    value= ""//, unit="Hertz"
+
+                }});
+            x.Attributes.Add("purchasable_offer", new object[] {
+                                        new{
+                                                currency= amazonConnection.GetCurrentMarketplace.CurrencyCode,
+                                                our_price=new []
+                                                          { new{
+                                                                    schedule =new[]
+                                                              { new {
+                                                                        value_with_tax= 59.99M
+                                                                    }
+                                                              }}
+                                                          }
+                                             }
+                                      });
+
+            x.Attributes.Add("fulfillment_availability",
+                new object[] { new { fulfillment_channel_code = "DEFAULT",
+                    quantity = 10 } });
+            //x.Attributes["vat_level"] = new[] { new {
+            //        value ="registration_not_required"//, unit="Hertz"
+
+            //   }};
+            x.Attributes.Add("condition_type", new[] { new {
+                    value= "new_new"//, unit="Hertz"
+
+                }});
+            x.Attributes.Remove("variation_theme");
+            x.Attributes.Remove("generic_keyword"); x.Attributes.Remove("vat_level");
+
+
+
+            // var r = new object[] { new{ value = DateTime.Now } };
+            // x.Attributes.Add("street_date", System.Text.Json.JsonSerializer.Serialize( new[] { new { value = DateTime.Now } }));//= System.Text.Json.JsonSerializer.Serialize(r);
+            var putrequest = new ListingsItemPutRequest()
+            {
+                attributes = x.Attributes,
+                productType = def.ProductType,
+                requirements = Requirements.LISTING
+            };
+
+            var y = amazonConnection.ListingsItem.PutListingsItem(
+                new ParameterPutListingItem()
+                {
+                    listingsItemPutRequest = putrequest,
+                    sku = "71683-001",
+                    sellerId = amazonConnection.GetCurrentSellerID
+                });
 
 
 
