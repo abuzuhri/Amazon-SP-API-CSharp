8bc3ceefb8b1ae2fe552ac5a2e2dcf8ab9821bfc
hyoungjin cho <hyoungjin.ch@codizsoft.com>
2023-05-08 14:19:40 -0700

    Converting value "IN" to FikaAmazonAPI.AmazonSpApiSDK.Models.FulfillmentInbound.UnitOfMeasurement Exception Fix for FulfillmentInbound getTransportDetails endpoint. Amazon returns 'inches' and 'in' for the PartneredSmallParcelData.PackagesList[0].Dimensions.Unit value.

---
+++ Source/FikaAmazonAPI/Utils/UnitOfMeasurementConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using FikaAmazonAPI.AmazonSpApiSDK.Models.FulfillmentInbound;
+
+namespace FikaAmazonAPI.Utils
+{
+    class UnitOfMeasurementConverter : StringEnumConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            string unitOfMeasurementString = reader.Value.ToString();
+            if (unitOfMeasurementString.Equals("in", StringComparison.OrdinalIgnoreCase))
+                return UnitOfMeasurement.Inches;
+            else
+                return (UnitOfMeasurement)Enum.Parse(typeof(UnitOfMeasurement), unitOfMeasurementString, true);
+        }
+    }
+}
