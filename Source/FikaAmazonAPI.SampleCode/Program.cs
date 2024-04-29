36431b6c1e94b1f57b0d044b505a7e52dd471268
Tareq Abuzuhri <tareq.abuzuhri@gmail.com>
2024-04-29 14:34:01 +0200

    Updated Sample

--- Source/FikaAmazonAPI.SampleCode/Program.cs
+++ Source/FikaAmazonAPI.SampleCode/Program.cs
@@ -1,7 +1,4 @@
-﻿using FikaAmazonAPI.ReportGeneration;
-using FikaAmazonAPI.Utils;
-using Microsoft.Extensions.Configuration;
-using static FikaAmazonAPI.Utils.Constants;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace FikaAmazonAPI.SampleCode
 {
@@ -30,58 +27,14 @@ namespace FikaAmazonAPI.SampleCode
             });
 
 
-            ReportManager reportManager = new ReportManager(amazonConnection);
-            //var datadddd = reportManager.GetFbaEstimateFeeData(DateTime.UtcNow.AddDays(-30), DateTime.UtcNow);
-            var dataddd222d = reportManager.GetReferralFeeReportData(DateTime.UtcNow.AddDays(-30), DateTime.UtcNow);
+            ReportManagerSample reportManagerSample = new ReportManagerSample(amazonConnection);
+            reportManagerSample.CallReport();
+            //var error = amazonConnection.Reports.CreateReportAndDownloadFile(Utils.Constants.ReportTypes.GET_STRANDED_INVENTORY_UI_DATA);
+            //var dddd = amazonConnection.Reports.CreateReportAndDownloadFile(Utils.Constants.ReportTypes.GET_FBA_MYI_ALL_INVENTORY_DATA);
+            //var dddd = amazonConnection.Reports.CreateReportAndDownloadFile(Utils.Constants.ReportTypes.GET_FBA_MYI_UNSUPPRESSED_INVENTORY_DATA);
+            //ReportManager reportManager = new ReportManager(amazonConnection);
 
-
-
-            var file22227 = amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_REFERRAL_FEE_PREVIEW_REPORT, DateTime.UtcNow.AddDays(-3), DateTime.UtcNow);
-
-
-            var file3337 = amazonConnection.Reports.GetReports(new Parameter.Report.ParameterReportList()
-            {
-                reportTypes = new ReportTypes[] { ReportTypes.GET_REFERRAL_FEE_PREVIEW_REPORT },
-            });
-
-            var sss = amazonConnection.Reports.GetReport("784871019793");
-
-
-            var file = amazonConnection.Reports.CreateReportAndDownloadFile(ReportTypes.GET_FBA_ESTIMATED_FBA_FEES_TXT_DATA, DateTime.UtcNow.AddDays(-30), DateTime.UtcNow);
-
-
-
-
-
-
-            //var feedTypes = new ParameterGetFeed { feedTypes = { FeedType.POST_PRODUCT_PRICING_DATA }, processingStatuses = ProcessingStatuses.IN_PROGRESS };
-
-            //var feeds = amazonConnection.Feed.GetFeeds(feedTypes);
-
-
-            //FeedsSample feedsSample = new FeedsSample(amazonConnection);
-            //feedsSample.SubmitFeedPRICING(69.3F, "8809606851663");
-
-            var feeds = amazonConnection.Feed.GetFeeds(new Parameter.Feed.ParameterGetFeed()
-            {
-                processingStatuses = ProcessingStatuses.IN_PROGRESS,//ProcessingStatuses.IN_QUEUE,
-                pageSize = 100,
-                feedTypes = new List<FeedType> { FeedType.POST_PRODUCT_PRICING_DATA },
-                marketplaceIds = new List<string> { MarketPlace.UnitedArabEmirates.ID }
-            });
-
-            var feeds2 = amazonConnection.Feed.GetFeeds(new Parameter.Feed.ParameterGetFeed()
-            {
-                processingStatuses = ProcessingStatuses.IN_PROGRESS,//ProcessingStatuses.IN_PROGRESS,
-                pageSize = 100,
-                feedTypes = new List<FeedType> { FeedType.POST_INVENTORY_AVAILABILITY_DATA },
-                marketplaceIds = new List<string> { MarketPlace.UnitedArabEmirates.ID }
-            });
-            foreach (var feed in feeds)
-            {
-                Console.WriteLine("FeedId " + feed.FeedId);
-                //var result = amazonConnection.Feed.CancelFeed(feed.FeedId);
-            }
+            //var dddddd = reportManager.GetAFNInventoryQtyAsync().ConfigureAwait(false).GetAwaiter().GetResult();
 
 
 
