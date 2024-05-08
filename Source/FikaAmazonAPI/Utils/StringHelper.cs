a746f4eeb9f181a1112766a3e8240a4c79759423
kevinvenclovas <venclovas.kevin@googlemail.com>
2024-05-08 20:24:59 +0200

    added camelcase support for url query parameters

---
+++ Source/FikaAmazonAPI/Utils/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FikaAmazonAPI.Utils
+{
+    public static class StringHelper
+    {
+        public static string ToCamelCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return str;
+
+            string[] words = str.Split(new char[] { '_', '-', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string camelCase = words[0].ToLower();
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                camelCase += words[i].Substring(0, 1).ToUpper() + words[i].Substring(1).ToLower();
+            }
+
+            return camelCase;
+        }
+    }
+}
