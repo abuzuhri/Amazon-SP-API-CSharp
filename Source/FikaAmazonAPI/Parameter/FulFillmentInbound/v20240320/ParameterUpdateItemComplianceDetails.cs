ebfdf76d32eb4b091950f2ed3d7ae1654f344673
Tareq Abuzuhri <tareq.abuzuhri@gmail.com>
2024-09-06 20:18:44 +0200

    change formate

--- Source/FikaAmazonAPI/Parameter/FulFillmentInbound/v20240320/ParameterUpdateItemComplianceDetails.cs
+++ Source/FikaAmazonAPI/Parameter/FulFillmentInbound/v20240320/ParameterUpdateItemComplianceDetails.cs
@@ -1,10 +1,9 @@
 ﻿using FikaAmazonAPI.Search;
-using FikaAmazonAPI.Parameter.FulFillmentInbound;
 
 namespace FikaAmazonAPI.Parameter.FulFillmentInbound.v20240320
 {
     public class ParameterUpdateItemComplianceDetails : ParameterBased
     {
-        public string MarketplaceId { get; set; }
+        public string marketplaceId { get; set; }
     }
 }
