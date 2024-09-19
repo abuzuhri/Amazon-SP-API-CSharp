7f89c0c586cae1da8840b17c8645d5fa53dd8824
TManITtech <TManITtech@gmail.com>
2024-09-19 17:42:01 -0400

    Added the missing getAdditionalInputs method/call of the Amazon Shipping API v2 which needs to be called in a case where requiresAdditionalInputs = true.

---
+++ Source/FikaAmazonAPI/AmazonSpApiSDK/Models/ShippingV2/GetAdditionalInputsRequest.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FikaAmazonAPI.AmazonSpApiSDK.Models.ShippingV2
+{
+    [DataContract]
+    public class GetAdditionalInputsRequest
+    {
+        /// <summary>
+        /// The request token returned in the response to the getRates operation.
+        /// </summary>
+        [DataMember(Name = "requestToken", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "requestToken")]
+        public string RequestToken { get; set; }
+
+        /// <summary>
+        /// The rate identifier for the shipping offering (rate) returned in the response to the getRates operation.
+        /// </summary>
+        [DataMember(Name = "rateId", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "rateId")]
+        public string RateId { get; set; }
+
+
+        /// <summary>
+        /// Get the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class " + nameof(GetAdditionalInputsRequest) + " {\n");
+            sb.Append($"  {nameof(RequestToken)}: ").Append(RequestToken).Append("\n");
+            sb.Append($"  {nameof(RateId)}: ").Append(RateId).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Get the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+
+}
--- Source/FikaAmazonAPI/AmazonSpApiSDK/Models/ShippingV2/GetAdditionalInputsResponse.cs
+++ Source/FikaAmazonAPI/AmazonSpApiSDK/Models/ShippingV2/GetAdditionalInputsResponse.cs
@@ -1,44 +1,47 @@
-using System;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
 using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Text;
 
-namespace FikaAmazonAPI.AmazonSpApiSDK.Models.ShippingV2 {
-
-  /// <summary>
-  /// The response schema for the getAdditionalInputs operation.
-  /// </summary>
-  [DataContract]
-  public class GetAdditionalInputsResponse {
-    /// <summary>
-    /// Gets or Sets Payload
-    /// </summary>
-    [DataMember(Name="payload", EmitDefaultValue=false)]
-    [JsonProperty(PropertyName = "payload")]
-    public GetAdditionalInputsResult Payload { get; set; }
-
+namespace FikaAmazonAPI.AmazonSpApiSDK.Models.ShippingV2
+{
 
     /// <summary>
-    /// Get the string presentation of the object
+    /// The response schema for the getAdditionalInputs operation.
     /// </summary>
-    /// <returns>String presentation of the object</returns>
-    public override string ToString()  {
-      var sb = new StringBuilder();
-      sb.Append("class GetAdditionalInputsResponse {\n");
-      sb.Append("  Payload: ").Append(Payload).Append("\n");
-      sb.Append("}\n");
-      return sb.ToString();
+    [DataContract]
+    public class GetAdditionalInputsResponse
+    {
+        /// <summary>
+        /// Gets or Sets Payload
+        /// See: https://developer-docs.amazon.com/amazon-shipping/docs/shipping-api-v2-reference#getadditionalinputsresponse
+        /// </summary>
+        [DataMember(Name = "payload", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "payload", Required = Required.AllowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public GetAdditionalInputsResult Payload { get; set; }
+
+
+        /// <summary>
+        /// Get the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class GetAdditionalInputsResponse {\n");
+            sb.Append("  Payload: ").Append(Payload).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Get the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
     }
 
-    /// <summary>
-    /// Get the JSON string presentation of the object
-    /// </summary>
-    /// <returns>JSON string presentation of the object</returns>
-    public string ToJson() {
-      return JsonConvert.SerializeObject(this, Formatting.Indented);
-    }
-
-}
 }
