e28aec8755a6a50dafa5a4735bd723e26f9fc041
Finn Reilly <finn.reilly@mountainwarehouse.com>
2024-12-04 17:04:07 +0000

    add factory class to explicitly create multiple connections for multithreading

---
+++ Source/FikaAmazonAPI/AmazonMultithreadedConnectionFactory.cs
@@ -0,0 +1,46 @@
+﻿using FikaAmazonAPI.Utils;
+using System.Globalization;
+
+namespace FikaAmazonAPI
+{
+    public class AmazonMultithreadedConnectionFactory
+    {
+        private readonly string _accessKey;
+        private readonly string _secretKey;
+        private readonly string _roleArn;
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly string _refreshToken;
+        private readonly string _proxyAddress;
+
+        private readonly IRateLimitingHandler _rateLimitingHandler;
+
+        public AmazonMultithreadedConnectionFactory(
+            string AccessKey,
+            string SecretKey,
+            string RoleArn,
+            string ClientId,
+            string ClientSecret,
+            string RefreshToken,
+            string ProxyAddress = null,
+            IRateLimitingHandler rateLimitingHandler = null)
+        {
+            _accessKey = AccessKey;
+            _secretKey = SecretKey;
+            _roleArn = RoleArn;
+            _clientId = ClientId;
+            _clientSecret = ClientSecret;
+            _refreshToken = RefreshToken;
+            _proxyAddress = ProxyAddress;
+
+            _rateLimitingHandler = rateLimitingHandler ?? new RateLimitingHandler();
+        }
+
+        public AmazonConnection RequestScopedConnection(string refCode = null, CultureInfo cultureInfo = null)
+        {
+            // need to create distinct credential/connection here so that token caching in credential is predicably kept within scope
+            var credential = new AmazonCredential(_accessKey, _secretKey, _roleArn, _clientId, _clientSecret, _refreshToken, _proxyAddress);
+            return new AmazonConnection(credential, _rateLimitingHandler, refCode, cultureInfo);
+        }
+    }
+}
