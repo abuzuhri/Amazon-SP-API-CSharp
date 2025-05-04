45c0bbd1cd669e3069eda474c5b3510408a95b82
kevinvenclovas <venclovas.kevin@googlemail.com>
2025-05-04 09:17:44 +0200

    Squashed commit of the following:
    commit f72094de8dd8b6a760d55d500d4eab7eee8274a9
    Author: kevinvenclovas <venclovas.kevin@googlemail.com>
    Date:   Fri Mar 7 20:50:22 2025 +0100
    
        fixed daterange split issue

--- Source/FikaAmazonAPI/ReportGeneration/ReportDateRange.cs
+++ Source/FikaAmazonAPI/ReportGeneration/ReportDateRange.cs
@@ -10,30 +10,27 @@ namespace FikaAmazonAPI.ReportGeneration
         public ReportDateRange(DateTime startDate, DateTime endDate) { StartDate = startDate; EndDate = endDate;}
 
 
-		public static IList<ReportDateRange> GetDateRange(DateTime startDate, DateTime endDate, int MaxDays)
-		{
-			List<ReportDateRange> list = new List<ReportDateRange>();
-			DateTime tempEnd = startDate;
-			DateTime start = startDate;
+        public static IList<ReportDateRange> GetDateRange(DateTime startDate, DateTime endDate, int maxDays)
+        {
+            List<ReportDateRange> list = new List<ReportDateRange>();
+            DateTime tempEnd;
+            DateTime start = startDate;
 
-			while (true)
-			{
-				tempEnd = tempEnd.AddDays(MaxDays);
-				if (tempEnd > endDate)
-				{
-					tempEnd = endDate;
-					list.Add(new ReportDateRange(start, tempEnd));
-					break;
-				}
-				else
-				{
-					list.Add(new ReportDateRange(start, tempEnd));
-					start = tempEnd.AddSeconds(1);
-				}
+            while (start < endDate)
+            {
+                tempEnd = start.AddDays(maxDays);
 
-			}
+                if (tempEnd > endDate)
+                {
+                    tempEnd = endDate;
+                }
 
-			return list;
+                list.Add(new ReportDateRange(start, tempEnd));
+
+                start = tempEnd.AddSeconds(1);
+            }
+
+            return list;
         }
     }
 }
