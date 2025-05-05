432b2364cf13110d37920350ed445c27f99e4f96
kevinvenclovas <venclovas.kevin@googlemail.com>
2025-05-05 21:00:38 +0200

    fixed some financial v20240619 issues

--- Source/FikaAmazonAPI/Parameter/Finance/ParameterListFinancialTransactions20240619.cs
+++ Source/FikaAmazonAPI/Parameter/Finance/ParameterListFinancialTransactions20240619.cs
@@ -5,7 +5,7 @@ namespace FikaAmazonAPI.Parameter.Finance
 {
     public class ParameterListFinancialTransactions20240619 : ParameterBased
     {
-        public DateTime? postedAfter { get; set; }
+        public DateTime postedAfter { get; set; }
         public DateTime? postedBefore { get; set; }
         public string? marketplaceId { get; set; }
         public string nextToken { get; set; }
