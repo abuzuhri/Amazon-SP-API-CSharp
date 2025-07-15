cf154f3c84de4d5b9f26a6b5e98f5d459150f2e0
Kevin Venclovas <k.venclovas@alfa-direkt.de>
2025-07-15 09:06:45 +0200

    fixed xml build issue

--- Source/FikaAmazonAPI/ConstructFeed/Messages/StandardPrice.cs
+++ Source/FikaAmazonAPI/ConstructFeed/Messages/StandardPrice.cs
@@ -1,11 +1,20 @@
-﻿using System.Xml.Serialization;
+﻿using System.Globalization;
+using System.Xml.Serialization;
 
 namespace FikaAmazonAPI.ConstructFeed.Messages
 {
     public class StandardPrice
     {
-        [XmlText]
+        [XmlIgnore]
         public decimal Value { get; set; }
+
+        [XmlText]
+        public string ValueString
+        {
+            get => Value.ToString(CultureInfo.InvariantCulture);
+            set => Value = decimal.Parse(value, CultureInfo.InvariantCulture);
+        }
+
         [XmlAttribute]
         public string currency { get; set; }
         public string start_at { get; set; }
