fdd4da3b3ea408dd3123bcb8694b370108d52f09
Madalin <babusca.madalin@gmail.com>
2025-12-23 23:33:46 +0200

    moved _allMarketplaces below the marketplaces, because static fields are initialized in the order they appear in the source code and allMarketplaces had null values

--- Source/FikaAmazonAPI/Utils/MarketPlace.cs
+++ Source/FikaAmazonAPI/Utils/MarketPlace.cs
@@ -25,33 +25,6 @@ namespace FikaAmazonAPI.Utils
 
         [JsonConstructorAttribute]
         public MarketPlace() { }
-        
-        private static readonly List<MarketPlace> _allMarketplaces = new List<MarketPlace>()
-        {
-            // NorthAmerica
-            US, Canada, Mexico, Brazil,
-            // Europe
-            Spain, UnitedKingdom, France, Belgium,
-            Netherlands, Germany, Italy, Sweden,
-            Egypt, Poland, Turkey, UnitedArabEmirates,
-            India, SaudiArabia, SouthAfrica, Ireland,
-            // FarEast
-            Singapore, Australia, Japan
-        };
-
-        public static MarketPlace GetMarketPlaceByID(string id)
-        {
-            var marketpalce = _allMarketplaces.FirstOrDefault(a => a.ID == id);
-            if (marketpalce == null)
-                throw new AmazonInvalidInputException($"InvalidInput, MarketPlace or MarketPlaceID cannot be null for both!");
-
-            return marketpalce;
-        }
-
-        public static MarketPlace GetMarketplaceByCountryCode(string countryCode)
-        {
-            return _allMarketplaces.FirstOrDefault(a => a.Country.Code == countryCode);
-        }
 
         //https://developer-docs.amazon.com/sp-api/docs/marketplace-ids
 
@@ -84,5 +57,32 @@ namespace FikaAmazonAPI.Utils
         public static readonly MarketPlace Singapore = new MarketPlace("A19VAU5U5O7RUS", Region.FarEast, Country.SG, BaseCurrencyCode.SGD);
         public static readonly MarketPlace Australia = new MarketPlace("A39IBJ37TRP1C6", Region.FarEast, Country.AU, BaseCurrencyCode.AUD);
         public static readonly MarketPlace Japan = new MarketPlace("A1VC38T7YXB528", Region.FarEast, Country.JP, BaseCurrencyCode.JPY);
+
+        private static readonly List<MarketPlace> _allMarketplaces = new List<MarketPlace>()
+        {
+            // NorthAmerica
+            US, Canada, Mexico, Brazil,
+            // Europe
+            Spain, UnitedKingdom, France, Belgium,
+            Netherlands, Germany, Italy, Sweden,
+            Egypt, Poland, Turkey, UnitedArabEmirates,
+            India, SaudiArabia, SouthAfrica, Ireland,
+            // FarEast
+            Singapore, Australia, Japan
+        };
+
+        public static MarketPlace GetMarketPlaceByID(string id)
+        {
+            var marketpalce = _allMarketplaces.FirstOrDefault(a => a.ID == id);
+            if (marketpalce == null)
+                throw new AmazonInvalidInputException($"InvalidInput, MarketPlace or MarketPlaceID cannot be null for both!");
+
+            return marketpalce;
+        }
+
+        public static MarketPlace GetMarketplaceByCountryCode(string countryCode)
+        {
+            return _allMarketplaces.FirstOrDefault(a => a.Country.Code == countryCode);
+        }
     }
 }
